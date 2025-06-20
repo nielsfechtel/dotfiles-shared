45b0163d1b458f9007fd3e2afd8d996182bf2af6
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2025-06-20 11:20:38 +0200

    feat: add hardtime.nvim plugin

---
+++ nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,20 @@
+print("notify_and_hardtime.lua loaded")
+
+return {
+  {
+    "m4xshen/hardtime.nvim",
+    lazy = false,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {},
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      vim.notify = require("notify")
+    end,
+  },
+}
