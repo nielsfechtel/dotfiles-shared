1d55cd47cfed2437c169196e0c85102edef5293b
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2025-06-22 12:23:32 +0200

    feat: add count to buffer-hotkeys

---
+++ nvim/lua/plugins/extend-bufferline.lua
@@ -0,0 +1,34 @@
+# Have buffer-switch-keys accept counts (e.g. 2L)
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    {
+      "L",
+      function()
+        vim.cmd("bnext " .. vim.v.count1)
+      end,
+      desc = "Next buffer",
+    },
+    {
+      "H",
+      function()
+        vim.cmd("bprev " .. vim.v.count1)
+      end,
+      desc = "Previous buffer",
+    },
+    {
+      "]b",
+      function()
+        vim.cmd("bnext " .. vim.v.count1)
+      end,
+      desc = "Next buffer",
+    },
+    {
+      "[b",
+      function()
+        vim.cmd("bprev " .. vim.v.count1)
+      end,
+      desc = "Previous buffer",
+    },
+  },
+}
